37e7ca94437ee2253153c179c73c061afddb935f
Patrick <patrickhladun@gmail.com>
2021-03-25 20:08:40 +0000

    Add scroll feature to autmatically scroll to section on click.
    - created scrollNavigation function to add scroll funcionality
    - dynamically add section ID's in makeNavigation function

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,16 +9,21 @@ const makeNavigation = () => {
     nav.classList.add('navigation');
 
     sections.forEach( (section) => {
+
+        console.log(section);
         const navItem = document.createElement('li');
         const navLink = document.createElement('a');
         const navTitle = section.getAttribute('data-section');
+        const navTitleDashed = navTitle.replaceAll(' ', '-').toLowerCase();
 
         const navItemClasses = navTitle.toLowerCase() === 'home' ? ['nav-item', 'nav-active'] : ['nav-item'];
         navItem.classList.add(...navItemClasses);
 
-        navLink.href = `#${navTitle.toLowerCase()}`;
+        navLink.href = `#${navTitleDashed}`;
         navLink.classList.add('nav-link');
         navLink.textContent = navTitle;
+
+        section.id = navTitleDashed;
 
         navItem.appendChild(navLink);
         fragment.appendChild(navItem);
@@ -26,9 +31,33 @@ const makeNavigation = () => {
 
     nav.appendChild(fragment);
     navContainer.appendChild(nav);
+
 };
 
+const scrollNavigation = () => {
+    const links = document.querySelectorAll(".nav-link");
+
+    for (const link of links) {
+        link.addEventListener( "click", clickHandler );
+    }
+
+    function clickHandler(e) {
+        e.preventDefault();
+
+        const href = this.getAttribute("href");
+        const offsetTop = document.querySelector(href).offsetTop;
+
+        scroll({
+            top: offsetTop,
+            behavior: "smooth"
+        });
+    }
+}
+
 makeNavigation();
+scrollNavigation();
+
+
 
 // const t1 = performance.now();
 // console.log(`Call to doSomething took ${t1 - t0} milliseconds.`);
