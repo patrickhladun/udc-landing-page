491d832a76e3999e6a49f1d738d895e9b6907510
Patrick <patrickhladun@gmail.com>
2021-03-26 21:42:00 +0000

    Add on-scroll class to the header on scroll.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,63 +1,74 @@
-// const t0 = performance.now();
+const t0 = performance.now();
 
-const makeNavigation = () => {
-    const sections =  document.querySelectorAll('[data-section]');
+const sections =  document.querySelectorAll('[data-section]');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
 
-    const navContainer = document.getElementById('navigation');
-    const fragment = document.createDocumentFragment();
-    const nav = document.createElement('ul');
-    nav.classList.add('navigation');
+const navContainer = document.getElementById('navigation');
+const fragment = document.createDocumentFragment();
+const nav = document.createElement('ul');
+nav.classList.add('navigation');
 
-    sections.forEach( (section) => {
+sections.forEach( (section) => {
+    const navItem = document.createElement('li');
+    const navLink = document.createElement('a');
+    const navTitle = section.getAttribute('data-section');
+    const navTitleDashed = navTitle.replaceAll(' ', '-').toLowerCase();
 
-        console.log(section);
-        const navItem = document.createElement('li');
-        const navLink = document.createElement('a');
-        const navTitle = section.getAttribute('data-section');
-        const navTitleDashed = navTitle.replaceAll(' ', '-').toLowerCase();
+    const navItemClasses = navTitle.toLowerCase() === 'home' ? ['nav-item', 'nav-active'] : ['nav-item'];
+    navItem.classList.add(...navItemClasses);
 
-        const navItemClasses = navTitle.toLowerCase() === 'home' ? ['nav-item', 'nav-active'] : ['nav-item'];
-        navItem.classList.add(...navItemClasses);
+    navLink.href = `#${navTitleDashed}`;
+    navLink.classList.add('nav-link', navTitleDashed);
+    navLink.textContent = navTitle;
 
-        navLink.href = `#${navTitleDashed}`;
-        navLink.classList.add('nav-link');
-        navLink.textContent = navTitle;
+    section.id = navTitleDashed;
 
-        section.id = navTitleDashed;
+    navItem.appendChild(navLink);
+    fragment.appendChild(navItem);
+} );
 
-        navItem.appendChild(navLink);
-        fragment.appendChild(navItem);
-    } );
+nav.appendChild(fragment);
+navContainer.appendChild(nav);
 
-    nav.appendChild(fragment);
-    navContainer.appendChild(nav);
+const links = document.querySelectorAll(".nav-link");
 
-};
-
-const scrollNavigation = () => {
-    const links = document.querySelectorAll(".nav-link");
-
-    for (const link of links) {
-        link.addEventListener( "click", clickHandler );
-    }
-
-    function clickHandler(e) {
-        e.preventDefault();
-
-        const href = this.getAttribute("href");
-        const offsetTop = document.querySelector(href).offsetTop;
-
-        scroll({
-            top: offsetTop,
-            behavior: "smooth"
-        });
-    }
+for (const link of links) {
+    link.addEventListener( "click", clickHandler );
 }
 
-makeNavigation();
-scrollNavigation();
+function clickHandler( e ) {
+    e.preventDefault();
+
+    const href = this.getAttribute("href");
+    const offsetTop = document.querySelector(href).offsetTop;
+
+    scroll({
+        top: offsetTop,
+        behavior: "smooth"
+    });
+}
+
+window.addEventListener( 'scroll', () => {
+    let scrollPosition = window.scrollY;
+    
+    const headerHeight = header.offsetHeight;
+    scrollPosition >= headerHeight ? body.classList.add('on-scroll') : body.classList.remove('on-scroll');
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionEl = section.id;
+        const navItemUpdate = document.querySelector(`.${sectionEl}`);
+        // console.log(`position: ${scrollPosition} sectionTop: ${sectionTop}`);
+        if(scrollPosition >= sectionTop) {
+            navItemUpdate.classList.add('is-active');
+        } else {
+            navItemUpdate.classList.remove('is-active');
+        }
+        
+    })
+});
 
 
-
-// const t1 = performance.now();
-// console.log(`Call to doSomething took ${t1 - t0} milliseconds.`);
+const t1 = performance.now();
+console.log(`Call to doSomething took ${t1 - t0} milliseconds.`);
