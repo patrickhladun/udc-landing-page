c7d27ca6f1f78853c08821c9e214d928af5d208d
Patrick <patrickhladun@gmail.com>
2021-03-27 11:36:18 +0000

    Add .is-active class when the section is in view.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,3 @@
-const t0 = performance.now();
-
 const sections =  document.querySelectorAll('[data-section]');
 const body = document.querySelector('body');
 const header = document.querySelector('.header');
@@ -9,7 +7,7 @@ const fragment = document.createDocumentFragment();
 const nav = document.createElement('ul');
 nav.classList.add('navigation');
 
-sections.forEach( (section) => {
+const createNavItem = (section) => {
     const navItem = document.createElement('li');
     const navLink = document.createElement('a');
     const navTitle = section.getAttribute('data-section');
@@ -25,50 +23,49 @@ sections.forEach( (section) => {
     section.id = navTitleDashed;
 
     navItem.appendChild(navLink);
-    fragment.appendChild(navItem);
+
+    return navItem;
+}
+
+const options = {
+    rootMargin: '-10px 0px -88%'
+};
+
+const navToggle = (entries) => {
+    entries.forEach(entry => {
+        const navLinkId = entry.target.id;
+        const navLink = document.querySelector(`.nav-item a.${navLinkId}`);
+        entry.isIntersecting ? navLink.classList.add('is-active') : navLink.classList.remove('is-active');
+    });
+}
+
+const observer = new IntersectionObserver(navToggle, options);
+
+sections.forEach( (section) => {
+    fragment.appendChild(createNavItem(section));
+    observer.observe(section);
 } );
 
 nav.appendChild(fragment);
 navContainer.appendChild(nav);
 
+window.addEventListener( 'scroll', () => {
+    let scrollPosition = window.scrollY;
+    const headerHeight = header.offsetHeight;
+    scrollPosition >= headerHeight ? body.classList.add('on-scroll') : body.classList.remove('on-scroll');
+});
+
 const links = document.querySelectorAll(".nav-link");
+for (const link of links) link.addEventListener( "click", clickHandler );
 
-for (const link of links) {
-    link.addEventListener( "click", clickHandler );
-}
-
-function clickHandler( e ) {
+function clickHandler(e) {
     e.preventDefault();
 
     const href = this.getAttribute("href");
-    const offsetTop = document.querySelector(href).offsetTop;
+    const offsetTop = document.querySelector(href).offsetTop + 70;
 
     scroll({
         top: offsetTop,
         behavior: "smooth"
     });
 }
-
-window.addEventListener( 'scroll', () => {
-    let scrollPosition = window.scrollY;
-    
-    const headerHeight = header.offsetHeight;
-    scrollPosition >= headerHeight ? body.classList.add('on-scroll') : body.classList.remove('on-scroll');
-
-    sections.forEach(section => {
-        const sectionTop = section.offsetTop;
-        const sectionEl = section.id;
-        const navItemUpdate = document.querySelector(`.${sectionEl}`);
-        // console.log(`position: ${scrollPosition} sectionTop: ${sectionTop}`);
-        if(scrollPosition >= sectionTop) {
-            navItemUpdate.classList.add('is-active');
-        } else {
-            navItemUpdate.classList.remove('is-active');
-        }
-        
-    })
-});
-
-
-const t1 = performance.now();
-console.log(`Call to doSomething took ${t1 - t0} milliseconds.`);
